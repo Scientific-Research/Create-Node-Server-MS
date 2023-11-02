c7ee7a382e8d9933653f99027e884da2f27800fe
Maximilian <scientificresearch309@gmail.com>
2023-11-02 13:29:18 +0100

    routing url === "/" created!

--- app.js
+++ app.js
@@ -4,8 +4,24 @@ const http = require("http");
 // http.createServer(function rqListener(req, res) {});
 // http.createServer(function (req, res) {});
 const server = http.createServer((req, res) => {
-  console.log(req.url, req.method, req.headers);
+  //   console.log(req.url, req.method, req.headers);
   //process.exit();
+  const url = req.url;
+  if (url === "/") {
+    res.write("<html>");
+    // res.write("<head><title>Enter Message:</title></head>");
+    res.write(
+      "<body>\n" +
+        "<h1>Enter your message in below field:</h1>\n" +
+        '<form action="/message" method="POST">\n' +
+        '<input type="text" name="message">\n' +
+        '<button type="submit">Send</button>\n' +
+        "</form>\n" +
+        "</body>\n"
+    );
+    res.write("</html>");
+    return res.end();
+  }
   res.setHeader("Content-Type", "text/html");
   res.write("<html>");
   res.write("<head><title>My First Page</title></head>");
