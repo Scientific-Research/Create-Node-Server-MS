6d99124d4a814ee9475d9dccfe3303b62c454bf8
Maximilian <scientificresearch309@gmail.com>
2023-11-02 11:07:25 +0100

    server was created!

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const http = require("http");
 // function rqListener(req, res) {}
 // http.createServer(function rqListener(req, res) {});
 // http.createServer(function (req, res) {});
-http.createServer((req, res) => {
+const server = http.createServer((req, res) => {
   console.log(req);
+  //process.exit();
 });
+
+server.listen(3000);
