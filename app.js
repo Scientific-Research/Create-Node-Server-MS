ce3afae64945c3648cb7ae3053d1c83a067914c5
Maximilian <scientificresearch309@gmail.com>
2023-11-03 14:28:28 +0100

    handling different Routes - "/" - add-products - contact-us, ...

--- app.js
+++ app.js
@@ -2,12 +2,24 @@ const express = require("express");
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("In the middleware");
+app.use("/", (req, res, next) => {
+  console.log("This is always running!");
+  //res.send("sdfsDFsdf");
   next();
 });
-app.use((req, res, next) => {
-  console.log("In another middleware");
+
+app.use("/add-products", (req, res, next) => {
+  res.send("<h1>The Add Product Page!</h1>");
+  console.log("add-product-page");
+  // next();
+});
+
+app.use("/contact-us", (req, res, next) => {
+  res.send("<h1>The Contact Us Page!</h1>");
+});
+
+app.use("/", (req, res, next) => {
+  console.log('This app.use with "/" must stay always as last Item!...');
   res.send("<h1>Hello from Express!</h1>");
 });
 
