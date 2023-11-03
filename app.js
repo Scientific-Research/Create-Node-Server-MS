1ede164ad25781f925c8a978ad4662a3ffca6f04
Maximilian <scientificresearch309@gmail.com>
2023-11-03 20:17:21 +0100

    Page not found was simuliert!

--- app.js
+++ app.js
@@ -9,4 +9,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(adminRoutes);
 app.use(shopRoutes);
 
+app.use("/", (req, res, next) => {
+  res.send("<h1>Page not found!</h1>");
+});
+
 app.listen(3000);
